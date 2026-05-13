9e46fa3fd888a861afb642bcdef545655013fd31
Marco Luzzara <marco.luzzara@hotmail.it>
2026-05-13 08:44:58 +0200

    fix "ip a flush dev lo" command during node boot

--- runtime/linux.tcl
+++ runtime/linux.tcl
@@ -576,22 +576,10 @@ proc runConfOnNode { node } {
     # change all occurrences of "dev lo0" to "dev lo"
     regsub -all {dev lo0} $bootcfg {dev lo} bootcfg
 
-    writeDataToFile $node_dir/$confFile [join "{ip a flush dev lo} $bootcfg" "\n"]
-    exec docker exec -i $node_id sh -c "cat > $confFile" < $node_dir/$confFile
-    exec echo "LOG START" > $node_dir/out.log
-    catch {exec docker exec $node_id $bootcmd $confFile >>& $node_dir/out.log} err
-    if { $err != "" } {
-	if { $execMode != "batch" } {
-	    after idle {.dialog1.msg configure -wraplength 4i}
-	    tk_dialog .dialog1 "IMUNES warning" \
-		"There was a problem with configuring the node [getNodeName $node] ($node_id).\nCheck its /$confFile and /out.log files." \
-	    info 0 Dismiss
-	} else {
-	    puts "IMUNES warning"
-	    puts "\nThere was a problem with configuring the node [getNodeName $node] ($node_id).\nCheck its /$confFile and /out.log files."
-	}
-    }
-    exec docker exec -i $node_id sh -c "cat > out.log" < $node_dir/out.log
+    set cfg [join "$bootcfg" "\n"]
+    writeDataToNodeFile $node /$confFile $cfg
+    set cmds "ip a flush dev lo ; $bootcmd /$confFile > /out.log 2> /err.log"
+    exec docker exec -i $node_id sh -c "$cmds"
 
     set nodeNs [getNodeNamespace $node]
     foreach ifc [allIfcList $node] {
